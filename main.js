7686141fb100a419a0dd087a85c721194449b736
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-04-06 01:49:30 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 1000,
+    backgroundColor: "#000000",
+    webPreferences: {
+      contextIsolation: true,
+    }
+  });
+
+  win.loadFile('index.html');
+
+  // Optional: open dev tools in dev
+  // win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
