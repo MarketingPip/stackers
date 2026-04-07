b56a24fb8f5116958158cbbb8682e0fd7b217d69
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-04-07 06:18:26 -0400

    Create settings.js

---
+++ src/settings.js
@@ -0,0 +1,14 @@
+const defaultSettings = {
+  sound_enabled: true,
+  highscore: 0,
+  fullscreen: false, // Sync default with launch flag
+  free_play: false, // if free play is true - do not show insert credits message. 
+  rotation: false,
+  grid_rows: 15,
+  grid_columns: 7,
+  major_prize_row: 0,
+  minor_prize_row: 4,
+  sfx_path: "https://lambda.vgmtreasurechest.com/soundtracks/stacker-arcade-gamerip-2004"
+};
+
+export default defaultSettings;
