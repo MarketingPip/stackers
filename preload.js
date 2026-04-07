d39d9ddb39ae724827e8e010f557a760e34e9214
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-04-06 21:38:42 -0400

    Update preload.js

--- preload.js
+++ preload.js
@@ -3,8 +3,17 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
   writeToFile: (filePath, content) => ipcRenderer.invoke('write-to-file', filePath, content),
-  call: (method, ...args) => ipcRenderer.invoke(method, args),
-  on: (callback, ...args) => ipcRenderer.on('emit', method, args)
+  call: (method, ...args) => ipcRenderer.invoke(method, ...args),
+  on: (channel, callback) => {
+    const newCallback = (event, ...args) => callback(...args);
+    ipcRenderer.on(channel, newCallback);
+
+    // High-quality tip: Return a cleanup function
+    // This allows you to stop listening later if needed
+    return () => {
+      ipcRenderer.removeListener(channel, newCallback);
+    };
+  },
 });
 
  
