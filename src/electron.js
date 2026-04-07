e5d7aa05b3c8b73a60cb6549fb33346343df652b
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-04-07 06:21:57 -0400

    Update electron.js

--- src/electron.js
+++ src/electron.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const fs = require('fs');
+const defaultSettings = require('./settings.js'); 
 
 let mainWindow;
 
@@ -25,18 +26,7 @@ if (!gotTheLock) {
 const USER_DATA_PATH = app.getPath('userData');
 
 // Default settings
-const defaultSettings = {
-  sound_enabled: true,
-  highscore: 0,
-  fullscreen: isArcadeMode, // Sync default with launch flag
-  free_play: false, // if free play is true - do not show insert credits message. 
-  rotation: false,
-  grid_rows: 15,
-  grid_columns: 7,
-  major_prize_row: 0,
-  minor_prize_row: 4,
-  sfx_path: "https://lambda.vgmtreasurechest.com/soundtracks/stacker-arcade-gamerip-2004"
-};
+defaultSettings.fullscreen = isArcadeMode; // Sync default with launch flag
 
 // emit events from nodejs back to HTML
 function emit(method, ...args) {
