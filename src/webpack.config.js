f880c945e968742a340b063bdc84555b52eeb1a3
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-04-07 01:28:16 -0400

    Update webpack.config.js

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlInlineScriptPlugin = require('html-inline-script-webpack-plugin');
+const HTMLInlineCSSWebpackPlugin = require('html-inline-css-webpack-plugin').default;
 
 module.exports = {
-  mode: 'development', // change to 'production' later
+  mode: 'development',
 
   entry: './src/game.js',
 
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'game.js',
+    filename: 'bundle.js', // This will be inlined and won't exist as a separate file in dist
     clean: true,
   },
 
@@ -18,18 +20,21 @@ module.exports = {
         test: /\.html$/,
         use: ['html-loader'],
       },
+      {
+        // Added CSS support so it can be extracted and inlined
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
 
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/game.html',
+      inject: 'body', // Ensures script is placed in the body for the inliner to find it
     }),
+    // Order matters: Inline CSS usually works best before or during the HTML script inlining
+    new HTMLInlineCSSWebpackPlugin(),
+    new HtmlInlineScriptPlugin(),
   ],
-
-  devServer: {
-    static: './dist',
-    open: true,
-    hot: true,
-  },
 };
